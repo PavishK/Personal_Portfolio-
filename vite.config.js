ec11d825c97b848fa42c44cca6e561cd40b9e2a4
PavishK <kpavish136@gmail.com>
2025-04-05 16:13:23 +0530

    Delploy lv4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-  },
-});
+  base: '/', // set to '/' for root deployment, or '/repo-name/' for GitHub Pages
+})
